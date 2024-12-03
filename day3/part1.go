0d39e56c491183b00a479f2a571092e4aa5a5484
ahmed-debbech <debbech.ahmed@gmail.com>
2024-12-03 23:16:49 +0100

    add day 3

--- day3/part1.go
+++ day3/part1.go
@@ -4,18 +4,24 @@ import (
 	"fmt"
 	"os"
 	"bufio"
+	"regexp"
+	"strings"
 )
 
 func getMul(lines []string) []string{
 	var st []string
 	for _, val := range lines {
-		for i:=0; i<=len(val)-12; i++{
+		for i:=0; i<=len(val)-4; i++{
 			if val[i:i+4] == "mul(" {
-				fmt.Println(val[i:i+12])
-				st = append(st, val[i:i+12])
+				//fmt.Println(val[i:i+12])
+				if i+12 > len(val)-1 {
+					st = append(st, val[i:len(val)-1])
+				}else{
+					st = append(st, val[i:i+12])
+				}
 			}
 		}
-		fmt.Println(st)
+		//fmt.Println(st)
 	}
 	return st
 }
@@ -23,11 +29,32 @@ func getMul(lines []string) []string{
 func verifyMuls(uncheckedMuls []string) []string{
 	var st []string
 	for _, vl := range uncheckedMuls {
-		if (vl[5] >= '0') && (vl[5] <= '9') {
-			if 
+		match, _ := regexp.MatchString(`mul\((\d{1,3}),(\d{1,3})\)`, vl)
+		if match {
+			st = append(st, vl)
 		}
 	}
+	
+	//trim
+	for i:=0; i<=len(st)-1; i++ {
+		st[i] = st[i][:strings.Index(st[i], ")")+1]
+		fmt.Println(st[i])
+
+	}
 	return st
+}
+
+func countMuls(muls []string) int64{
+	
+	var count int64 = 0
+	for _, vl := range muls {
+		var left int64
+		var right int64
+		fmt.Sscanf(vl, "mul(%d,%d)", &left, &right)
+		fmt.Println("ll : ", left)
+		count += (left * right)
+	}
+	return count
 }
 
 func main(){
@@ -45,5 +72,6 @@ func main(){
 
 	muls := getMul(lines)
 	muls = verifyMuls(muls)
-	fmt.Println(muls)
+	countFinal := countMuls(muls)
+	fmt.Println("THE FINAL COUNT OF MULS IS:",countFinal)
 }
