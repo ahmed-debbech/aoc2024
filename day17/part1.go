1bcad34c8028e8666e20e3e97c00d42e1fd178fb
ahmed-debbech <debbech.ahmed@gmail.com>
2024-12-18 15:35:51 +0100

    solve part1 day17

---
+++ day17/part1.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+	"strings"
+	"strconv"
+	"math"
+)
+
+
+var (
+	ops_names = []string{"adv", "bxl", "bst", "jnz", "bxc", "out", "bdv", "cdv"}
+)
+
+func main(){
+	lines := readFile()
+	//fmt.Println(lines)
+	a,b,c, ops := makeItReal(lines)
+	//fmt.Println(a,b,c,ops)
+
+	cpu := Cpu{}
+	cpu.Init(a,b,c,ops)
+
+	fmt.Println(cpu)
+
+	for cpu.Execute() {
+		cpu.Dump()
+	} 
+
+	fmt.Println("SEQ: ", cpu.Log)
+
+}
+
+type Cpu struct {
+	A int
+	B int
+	C int 
+	Pointer int
+	PIncr int 
+	Ops []int
+	Log []string
+}
+
+func (cpu *Cpu) Dump(){
+	fmt.Println("A:", cpu.A)
+	fmt.Println("B:", cpu.B)
+	fmt.Println("C:", cpu.C)
+}
+func (cpu *Cpu) Init(a int, b int, c int, ops []int){
+	cpu.A = a
+	cpu.B = b
+	cpu.C = c
+	cpu.Ops = ops
+	cpu.Pointer = 0
+	cpu.PIncr = 2
+	cpu.Log = make([]string, 0)
+}
+func (cpu *Cpu) Execute() bool{
+	if (cpu.Pointer >= len(cpu.Ops))  {return false}
+	
+	operation := cpu.Ops[cpu.Pointer]
+	operand := cpu.Ops[cpu.Pointer+1]
+	
+	cpu.Do(operation, operand)
+
+	if cpu.PIncr == 0 {
+		cpu.PIncr = 2
+		return true
+	}
+	cpu.Pointer += cpu.PIncr
+	return true
+}
+
+func (cpu *Cpu) Do(operation int, operand int){
+	//fmt.Println(operation, operand)
+	unit := cpu.KnowOperation(operation)
+	unit(operation, operand)
+}
+
+func (cpu *Cpu) KnowOperation(operation int) func(int, int) {
+	ss := ops_names[operation]
+
+	switch(ss){
+	case "adv":
+		return func(op int, oper int){
+			oper = cpu.setOperandRealValue(oper)
+			fmt.Println("adv ecuteing")
+			res := (cpu.A / int(math.Pow(2, float64(oper))))
+			cpu.A = int(res)
+		}
+	case "bxl":
+		return func(op int, oper int){
+			fmt.Println("bxl ecuteing")
+			res := (cpu.B ^ oper)
+			cpu.B = res
+		}
+	case "bst":
+		return func(op int, oper int){
+			oper = cpu.setOperandRealValue(oper)
+			fmt.Println("bst ecuteing")
+			g := oper % 8
+			res := 7 & g
+			cpu.B = res
+		}
+	case "jnz":
+		return func(op int, oper int){
+			fmt.Println("jnz ecuteing")
+			if cpu.A == 0 {return}
+			cpu.Pointer = oper
+			cpu.PIncr = 0
+		}
+	case "bxc":
+		return func(op int, oper int){
+			fmt.Println("bxc ecuteing")
+			res := (cpu.B ^ cpu.C)
+			cpu.B = res
+		}
+	case "out":
+		return func(op int, oper int){
+			fmt.Println("out ecuteing")
+			oper = cpu.setOperandRealValue(oper)
+			res := oper % 8
+			fop := strconv.Itoa(res)
+			cpu.Log = append(cpu.Log, (fop))
+		}
+	case "bdv":
+		return func(op int, oper int){
+			fmt.Println("bdv ecuteing")
+			oper = cpu.setOperandRealValue(oper)
+			fmt.Println("adv ecuteing")
+			res := (cpu.A / int(math.Pow(2, float64(oper))))
+			cpu.B = int(res)
+		}
+	case "cdv":
+		return func(op int, oper int){
+			fmt.Println("cdv ecuteing")
+			oper = cpu.setOperandRealValue(oper)
+			fmt.Println("adv ecuteing")
+			res := (cpu.A / int(math.Pow(2, float64(oper))))
+			cpu.C = int(res)
+		}
+	}
+	return nil
+}
+
+func (cpu *Cpu) setOperandRealValue(oper int) int{
+	if (oper <= 3) && (oper >= 0) {
+		return oper
+	}
+	if oper == 4 {
+		return cpu.A
+	}
+	if oper == 5 {
+		return cpu.B
+	}
+	if oper == 6 {
+		return cpu.C
+	}
+	return -1
+}
+
+
+func makeItReal(lines []string) (int,int,int, []int){
+	a, _ := strconv.Atoi(lines[0])
+	b, _ := strconv.Atoi(lines[1])
+	c, _ := strconv.Atoi(lines[2])
+
+	ops := make([]int, 0)
+	for _ , x := range strings.Split(lines[3], ",") {
+		xx , _ := strconv.Atoi(x)
+		ops = append(ops, xx)
+	}
+
+	return a,b,c, ops
+}
+
+func readFile() []string{
+	file, _ := os.Open("input")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	scanner.Scan()
+	lines = append(lines, strings.Trim(strings.Split(scanner.Text(), ":")[1], " "))
+	scanner.Scan()
+	lines = append(lines, strings.Trim(strings.Split(scanner.Text(), ":")[1], " "))
+	scanner.Scan()
+	lines = append(lines, strings.Trim(strings.Split(scanner.Text(), ":")[1]," "))
+	scanner.Scan()
+	scanner.Scan()
+	lines = append(lines, strings.Trim(strings.Split(scanner.Text(), ":")[1], " "))
+
+	//fmt.Println(lines)
+	return lines
+}
